70f452c5a4fb188100aab1401cb7c42357f4740f
tachiyoru <shanley@hotmail.fr>
2026-02-09 16:52:36 +0100

    step 4 - user logic - register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import LoginPage from './pages/Login.jsx';
+import RegisterPage from './pages/Register.jsx'
 
 function App() {
   return (
@@ -14,12 +15,13 @@ function App() {
             <Routes>
               <Route path="/" element={<div>Home Page</div>} />
               <Route path="/login" element={<LoginPage />} />
-              {/* <Route path="/register" element={<RegisterPage />} />
-              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-              <Route path="/reset-password" element={<ResetPasswordPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/logout" element={<LogoutPage />} />
-              <Route path="/auth/callback" element={<AuthCallbackPage />} /> */}
+              <Route path="/register" element={<RegisterPage />} />
+              {/* <Route path="/forgot-password" element={<ForgotPasswordPage />} /> */}
+              {/* <Route path="/email-confirmation" element={<EmailConfirmation />} /> */}
+              {/* <Route path="/reset-password" element={<ResetPasswordPage />} /> */}
+              {/* <Route path="/profile" element={<ProfilePage />} /> */}
+              {/* <Route path="/logout" element={<LogoutPage />} /> */}
+              {/* <Route path="/auth/callback" element={<AuthCallbackPage />} /> */}
             </Routes>
           </main>
           <footer>
