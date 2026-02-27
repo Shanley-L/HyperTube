7c52215f275ce8e567d1870600863c3a48f00a36
Jim LEROUX <jileroux@f4r10s8.clusters.42paris.fr>
2026-02-27 16:38:35 +0100

    search & select in progress

--- frontend/src/pages/movies.jsx
+++ frontend/src/pages/movies.jsx
@@ -8,11 +8,24 @@ export default function MovieTest() {
   const handleSearch = async () => {
     try {
       const res = await api.get(`movies/search?q=${query}`);
+      console.log(movies)
       setMovies(res.data);
     } catch (err) {
       console.error("Erreur de recherche", err);
     }
   };
+
+  const handleMovieClick = async (movieId) => {
+    try {
+      const response = await api.post('movies/select', {
+            selectMovieid: movieId // L'ID passe dans le body
+        });
+      console.log(response.data);
+      
+    } catch (error) {
+      console.error("Error while fetching movie")
+    }
+  }
 
   return (
     <div style={{ padding: '20px' }}>
@@ -26,9 +39,14 @@ export default function MovieTest() {
       <ul>
         {movies.map((movie, index) => (
           <li key={index}>
-            <strong>{movie.title}</strong> - Source: {movie.tracker}
-            <br />
-            <small>Magnet: {movie.magnet?.substring(0, 50)}...</small>
+            <img 
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+              alt={movie.title}
+              onClick={() => handleMovieClick(movie.id)}
+              className="rounded-lg shadow-md"
+              style={{ maxWidth: '300px', width: '100%', height: 'auto' }}
+            />
+            <strong>{movie.title}</strong>
           </li>
         ))}
       </ul>
