c3edce41f68c448ac2daa8004b744230d3259448
Man-shaa <manuel.sharifi@gmail.com>
2026-03-12 15:17:57 +0100

    [FIXED] middleware for /poster

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,15 +1,18 @@
-import express from 'express';
-import authMiddleware from '../middlewares/auth.js';
-import moviesController from '../controllers/moviesController.js';
+import express from "express";
+import authMiddleware from "../middlewares/auth.js";
+import moviesController from "../controllers/moviesController.js";
 
 const router = express.Router();
 
-router.get('/discover', authMiddleware, moviesController.discover)
-router.get('/search', authMiddleware, moviesController.search)
-router.post('/select', authMiddleware, moviesController.select)
-router.get('/posters', authMiddleware, moviesController.getPosters);
-router.post('/watched', authMiddleware, moviesController.watched);
-router.get('/getwatchedmovies', authMiddleware, moviesController.getWatchedMovies)
+router.get("/discover", authMiddleware, moviesController.discover);
+router.get("/search", authMiddleware, moviesController.search);
+router.post("/select", authMiddleware, moviesController.select);
+router.get("/posters", moviesController.getPosters);
+router.post("/watched", authMiddleware, moviesController.watched);
+router.get(
+  "/getwatchedmovies",
+  authMiddleware,
+  moviesController.getWatchedMovies,
+);
 
 export default router;
-
